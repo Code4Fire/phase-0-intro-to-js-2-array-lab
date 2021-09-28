651d4796867defa349b434745569cec9fd4f1e4c
Tiffany_Tam <tamtiffany89@gmail.com>
2021-09-28 16:44:12 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"];
+    
+    function destructivelyAppendCat(Ralph) {
+        cats.push("Ralph");
+        return cats;
+    }
+    function destructivelyPrependCat(Bob) {
+        cats.unshift("Bob");
+    }
+    function destructivelyRemoveLastCat() {
+        cats.pop();
+    }
+    function destructivelyRemoveFirstCat() {
+        cats.shift();
+    }
+    function appendCat(Broom)   {
+        let copyCats= [...cats, "Broom"];
+        return copyCats;
+       }
+    function prependCat(Arnold) {
+        let copyCats= ["Arnold", ...cats];
+        return copyCats;
+       }
+    function removeLastCat()   {
+        let copyCats= cats.slice(0, cats.length-1);
+        return copyCats;
+       }
+    function removeFirstCat() {
+        let copyCats= cats.slice(1);
+        return copyCats;
+       }
